62aa5e91fe02221a9308895139a5a491cd083ef2
alissondemidio <68760800+azzb10@users.noreply.github.com>
2021-11-15 14:38:59 -0300

    Rename and Icons

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import Main from './Main';
 import AddItem from './AddItem';
 import Statistics from './Statistics';
+const Senha = 'MSC4A325ba325B';
 
-export {Main, AddItem, Statistics};
+export {Main, AddItem, Statistics, Senha};
